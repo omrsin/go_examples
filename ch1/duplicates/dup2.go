10d87d47d38719efbd7e2eac661ff5029f70236e
Omar Erminy <omrsin@gmail.com>
2018-07-16 21:38:57 +0200

    Add the first examples for cheking duplicate lines

---
+++ ch1/duplicates/dup2.go
@@ -0,0 +1,42 @@
+// Dup2 prints the text of each line that appears more than
+// once in the input, preceeded by its count. It reas from
+// standard in or from a list of files
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v: %v\n", os.Args[0], err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
